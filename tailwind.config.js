79e6a17af3ae089767be0fe5a6801fd479cf309a
Hellstaff8822 <fgjd.gh50@gmail.com>
2025-08-20 15:21:14 +0300

    feat: add Tailwind, migrate header & footer to Hugo partials

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./site/layouts/**/*.html",
+		"./site/content/**/*.{html,md}",
+		"./src/**/*.{js,ts,css,scss}",
+	],
+	theme: { extend: {} },
+	plugins: [],
+}; 
