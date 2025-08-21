fd320e481b4f9b9cab963bada1335095331e599a
WILSONPLUR <yourshows12@gmail.com>
2025-08-21 13:00:46 +0300

    feat: add new section for main page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/tailwind.css";
 import "./css/main.scss";
 
+// Initialize mobile menu
 const mobileMenu = document.querySelector("[data-mobile-menu]");
 const nav = document.querySelector("[data-nav]");
 
@@ -9,6 +10,51 @@ function toggleMobileMenu() {
 }
 
 mobileMenu.addEventListener("click", toggleMobileMenu);
+
+// Initialize Swiper slider for products section
+function initProductsSwiper() {
+  if (typeof Swiper !== 'undefined') {
+    const swiper = new Swiper('.swiper-container', {
+      spaceBetween: 10,
+      slidesPerView: 2,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next-custom',
+        prevEl: '.swiper-button-prev-custom',
+      },
+    });
+  }
+}
+
+// Initialize Swiper slider for trusted by section
+function initTrustedSwiper() {
+  if (typeof Swiper !== 'undefined') {
+    const swiper = new Swiper('.swiper-container-trusted', {
+      modules: [Swiper.Pagination],
+      spaceBetween: 30,
+      slidesPerView: 4,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination-trusted',
+        clickable: true,
+        bulletClass: 'swiper-pagination-bullet-custom',
+        bulletActiveClass: 'swiper-pagination-bullet-active-custom',
+      },
+    });
+  }
+}
+
+// Initialize all Swipers
+function initSwipers() {
+  initProductsSwiper();
+  initTrustedSwiper();
+}
+
+// Initialize Swipers when DOM is loaded
+document.addEventListener('DOMContentLoaded', initSwipers);
+
+// Also initialize Swipers after page load to ensure all resources are loaded
+window.addEventListener('load', initSwipers);
 
 // Say hello
 // eslint-disable-next-line no-console
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,17 @@ module.exports = {
 		"./site/content/**/*.{html,md}",
 		"./src/**/*.{js,ts,css,scss}",
 	],
-	theme: { 
-	extend: {
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+				montserrat: ['Montserrat', 'sans-serif'],
+				unbounded: ['Unbounded', 'sans-serif'],
+			},
+		},
 		fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-        unbounded: ['Unbounded', 'sans-serif'],
-      },
-	} },
+			sans: ['Montserrat', 'sans-serif'],
+		},
+	},
 	plugins: [],
-}; 
+};
